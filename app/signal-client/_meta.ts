b8241ee16e3070deec3b4b43773933970e393011
yzqzy <yueluo.yang@gmail.com>
2025-12-04 21:46:16 +0800

    fix: 修复信号客户端和使用指南中的定价信息位置

--- app/signal-client/_meta.ts
+++ app/signal-client/_meta.ts
@@ -3,7 +3,7 @@ export default {
   download: "下载",
   "getting-started": "快速开始",
   features: "功能特性",
-  pricing: "会员定价",
   guides: "使用指南",
-  methodology: "方法论"
+  methodology: "方法论",
+  pricing: "会员定价"
 };
--- app/signal-client/guides/_meta.ts
+++ app/signal-client/guides/_meta.ts
@@ -1,8 +1,8 @@
 export default {
   dashboard: "看板",
   hotmap: "热图",
-  "stock-screener": "股票筛选器",
   "etf-screener": "ETF筛选器",
+  "stock-screener": "股票筛选器",
   "stock-analysis": "个股分析",
   "shareholder-analysis": "股东分析",
   "topic-library": "题材库",
