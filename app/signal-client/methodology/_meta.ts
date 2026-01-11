ed9e04fb38d8c0306b986bca257362bcb85acd88
yzqzy <yueluo.yang@gmail.com>
2026-01-11 20:54:13 +0800

    feat(文档): 添加 K线图使用指南，更新投资理念页面链接

--- app/signal-client/methodology/_meta.ts
+++ app/signal-client/methodology/_meta.ts
@@ -1,7 +1,7 @@
 export default {
   "investment-philosophy": "投资理念",
+  "kline-analysis": "K线图使用指南",
   "indicator-guide": "指标使用指南",
   "market-indicators": "市场指标分析",
   "filter-presets": "预设条件使用"
 };
-
