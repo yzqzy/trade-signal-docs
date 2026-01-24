e02b9dc67302c208da6c9d6d4f16703b3eb9fc28
yzqzy <yueluo.yang@gmail.com>
2026-01-24 20:20:39 +0800

    feat(投资指南): 添加投资术语解释和财报阅读基础页面

--- app/investment-guide/basics/_meta.ts
+++ app/investment-guide/basics/_meta.ts
@@ -1,4 +1,6 @@
 export default {
+  glossary: "投资术语解释",
+  "financial-statements": "财报阅读基础",
   stock: "股票基础",
   etf: "ETF基础"
 };
--- app/investment-guide/beginner-guide/_meta.ts
+++ app/investment-guide/beginner-guide/_meta.ts
@@ -1,5 +1,6 @@
 export default {
   "how-to-start": "如何开始投资",
   "common-mistakes": "新手常犯的错误",
-  "preparation": "开始投资前的准备"
+  "preparation": "开始投资前的准备",
+  "investment-planning": "如何制定投资计划"
 };
